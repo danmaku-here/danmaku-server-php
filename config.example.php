afc3fd50402fc95b1c16a7e6f79b782916492b2a
zsh2517 <865564024@qq.com>
2019-12-29 13:54:10 +0800

    忘了config...

---
+++ config.example.php
@@ -0,0 +1,8 @@
+<?php
+// 这里是一些设置
+$url="http://example.com/back/";
+// 这个项目的根URL（及exmaple.com/.../config.php所在的地址）
+$web="http://example.com/front/";
+// 前端使用的地址
+$passwd="cptbtptpdcptdtptp";
+// 添加用户使用的密码
